7218a672247f8d996a7dc6d04f02bf9327ccf34e
AnupKM <junk_magar@ymail.com>
2026-05-04 15:08:13 -0400

    backend - add cors

--- telemed-backend/src/main/java/com/telemed/backend/security/SecurityConfig.java
+++ telemed-backend/src/main/java/com/telemed/backend/security/SecurityConfig.java
@@ -59,7 +59,9 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of("*"));
+        configuration.setAllowedOriginPatterns(List.of( "http://localhost:4200",
+                "http://192.168.1.155:4200",
+                "https://d0e314a4.telemed-humla.pages.dev"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Cache-Control"));
         configuration.setAllowCredentials(true);
